117c628d0bb7be4af85efe40ab9d656656dace43
Vivek Pachauri <vivek.pachauri@rallyhealth.com>
2017-09-20 23:00:05 +0000

    uncommenting code

--- script.sh
+++ script.sh
@@ -4,8 +4,7 @@ echo "starting script to create a seed admin user in authn and starship"
 echo 'calling authn to create a new account'
 export PGPASSFILE=.pgpass
 email="initial2@init.com"
-#remove this
-#curl -X POST -H "Content-Type: application/json" -d '{"email" : "initial2@init.com","password" : "P@ssw0rd","confirmPassword" : "P@ssw0rd","username" : "initial2","acceptedHipaa": true,"acceptedToS": true}' https://accounts.icanhazevolutions.rally-dev.plumbing/auth/v1/register -k -v
+curl -X POST -H "Content-Type: application/json" -d '{"email" : "initial2@init.com","password" : "P@ssw0rd","confirmPassword" : "P@ssw0rd","username" : "initial2","acceptedHipaa": true,"acceptedToS": true}' https://accounts.icanhazevolutions.rally-dev.plumbing/auth/v1/register -k -v
 echo "authn call finished"
 echo "---------------------------------------------------"
 echo
@@ -23,31 +22,29 @@ else
     exit;
 fi
 echo "---------------------------------------------------"
-#remove from here till ******
-#echo
-#echo "updating role for user $rallyid"
-#echo "passing the following query to psql"
-#echo "update accounts set role='superuser' where id='$rallyid';" | psql -h postgres-rds-icanhazevolutions.cpirb098hibl.us-east-1.rds.amazonaws.com --username=authn
-#echo "psql call complete"
-#echo "---------------------------------------------------"
-#echo
-#echo "updating starship database to add this rallyid"
-#echo "generating json command with this rally id and email"
-#source vars.sh $rallyid $email > jsonScript.json
-#source rest.sh >> jsonScript.json
-#echo "json command generated"
-#cat jsonScript.json
-#echo "calling mongo with this script"
-#mongo mongo.icanhazevolutions.rally-dev.plumbing:27017/starship -u starship -p SOh3TbYhyuLiW8ypJPxmt2oOfL jsonScript.json
-#echo "mongo updated"
-#echo "---------------------------------------------------"
-#echo
-#echo "resetting role to user before ending the script"
-#echo "NOTE - this step is to be done during development only"
-#echo "once the script is ready this step should be removed"
-#echo "update accounts set role='user' where id='$rallyid';" | psql -h postgres-rds-icanhazevolutions.cpirb098hibl.us-east-1.rds.amazonaws.com --username=authn
-#echo "removing the generated script file"
-#rm jsonScript.json
-#******
+echo
+echo "updating role for user $rallyid"
+echo "passing the following query to psql"
+echo "update accounts set role='superuser' where id='$rallyid';" | psql -h postgres-rds-icanhazevolutions.cpirb098hibl.us-east-1.rds.amazonaws.com --username=authn
+echo "psql call complete"
+echo "---------------------------------------------------"
+echo
+echo "updating starship database to add this rallyid"
+echo "generating json command with this rally id and email"
+source vars.sh $rallyid $email > jsonScript.json
+source rest.sh >> jsonScript.json
+echo "json command generated"
+cat jsonScript.json
+echo "calling mongo with this script"
+mongo mongo.icanhazevolutions.rally-dev.plumbing:27017/starship -u starship -p SOh3TbYhyuLiW8ypJPxmt2oOfL jsonScript.json
+echo "mongo updated"
+echo "---------------------------------------------------"
+echo
+echo "resetting role to user before ending the script"
+echo "NOTE - this step is to be done during development only"
+echo "once the script is ready this step should be removed"
+echo "update accounts set role='user' where id='$rallyid';" | psql -h postgres-rds-icanhazevolutions.cpirb098hibl.us-east-1.rds.amazonaws.com --username=authn
+echo "removing the generated script file"
+rm jsonScript.json
 echo "script complete"
 echo "==================================================="
